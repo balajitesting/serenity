cc2636247d1ca55d3ae34e6373a642a73a31b89a
balaji.rachamadugu <balaji.rachamadugu@QT-INBG-LT0558.QUALITEST.LOCAL>
2020-04-29 14:52:25 +0530

    Pushing Runner File

--- src/test/java/com/project/runner/Runner.java
+++ src/test/java/com/project/runner/Runner.java
@@ -5,7 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-//@CucumberOptions(features= {"src/test/resources/features/BusinessProcessScenario.feature",},glue = {"com.project.teststeps"})
-@CucumberOptions(features= {"src/test/resources/features",},glue = {"com.project.teststeps" },tags= {"@Test=2"})
+@CucumberOptions(features= {"src/test/resources/features/BusinessProcessScenario.feature",},glue = {"com.project.teststeps"})
+//@CucumberOptions(features= {"src/test/resources/features",},glue = {"com.project.teststeps" },tags= {"@Test=2"})
 public class Runner  {}
 
